a3f8576fa0ecdf5b96234c18b4042ce7505d3530
magmafellow <magmafellow@gmail.com>
2024-04-03 21:37:41 +0300

    night changes 3.4.2024

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,39 @@
-import Link from "next/link"
+import Link from "next/link";
+import Post from "@/app/ui/Post";
 
 export default function Page() {
+  const arr3 = [1, 2, 3];
+
   return (
-    <div>
-      Go to <Link href="/login">login</Link> page.
-    </div>
-  )
+    <>
+      {/* header */}
+      <div className="container m-auto">
+        <header className="pt-11">
+          <nav className="flex justify-between">
+            <a href="/">Logo</a>
+            <ul className="flex justify-end gap-20">
+              <li>
+                <a href="#">Main</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Contacts</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </div>
+      {/* Main section */}
+      <main>
+        <div className="container xl:border-x border-neutral-600 m-auto min-h-72">
+          {/* wrapper for posts */}
+          <ul className="text-center">{
+            arr3.map((_i, i) => <li key={i}><Post /></li>)
+          }</ul>
+        </div>
+      </main>
+    </>
+  );
 }
---
+++ app/ui/Post.tsx
@@ -0,0 +1,22 @@
+export default function Post() {
+  return (
+    <div className="max-w-[550px] inline-block mb-10 border-b-stone-400 border-b-2 pb-2">
+      {/* time */}
+      <div className="text-end text-neutral-500">10:37</div>
+      <h3 className="text-xl font-bold">Fast cars</h3>
+      <p className="antialiased text-neutral-700 tracking-wide">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur magnam
+        porro est iste veniam earum odio. Nisi eum soluta incidunt, vitae
+        officiis optio molestias in maiores minima vel similique nesciunt quod
+        hic fugiat sint iure dignissimos accusamus voluptas expedita inventore
+        saepe! Ut, minima, quo quaerat a autem dicta eaque earum illum dolorum,
+        quas laborum cum fugiat molestias iure architecto. Odio sed labore
+        cumque architecto ducimus voluptates iste eum quidem quaerat qui debitis
+        nulla beatae rem eos explicabo doloribus maxime dignissimos in,
+        excepturi, iusto voluptate eveniet libero! Sapiente, magnam aperiam at
+        quam iste, et laborum cumque, sed exercitationem sit praesentium
+        explicabo?
+      </p>
+    </div>
+  );
+}
